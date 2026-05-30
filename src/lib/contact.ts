a5a1f6244b8e58edf56e6ae9565ac18e13d162d2
serviciosmedicosmb <serviciosmedicosmb@gmail.com>
2026-05-30 18:54:31 -0400

    Update contact.ts

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,5 +1,5 @@
-export const WHATSAPP_NUMBER = "56965824407";
+export const WHATSAPP_NUMBER = "56982752313";
 export const EMAIL = "serviciosmedicosmb@gmail.com";
 
-export const waLink = (msg: string = "Hola, necesito información sobre atención médica a domicilio.") =>
+export const waLink = (msg: string = "Hola, necesito información sobre sus servicios a domicilio.") =>
   `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(msg)}`;
